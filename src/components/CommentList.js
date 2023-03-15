83a5dac7f6334dfc35d29eaa7216458f8280fc99
Alvaro-LGPA <alvarolaura@gmail.com>
2023-03-15 09:46:16 +0100

    Refactor map to access comment array elements

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Comment from './Comment';
 
 export default function CommentList({ comments }) {
+  console.log(comments.comments)
   if (!comments) {
     return null;
   }
   
   return (
     <ul className='comments-list'>
-      {comments.map(comment => <Comment comment={comment} />)}
+      {comments.comments.map(comment => <Comment comment={comment} />)}
     </ul>
   );
 }
