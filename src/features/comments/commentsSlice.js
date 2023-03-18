f78bf2325906bb69aa3f4bcd4d90ee57f86807bc
Alvaro-LGPA <alvarolaura@gmail.com>
2023-03-18 20:05:43 +0100

    Properly write extrareducer for fulfilled. Cleanup

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -4,7 +4,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const loadCommentsForArticleId = createAsyncThunk(
   'comment/loadCommentsForArticleId',
-  async(id)=>{
+  async (id) => {
     const response = await fetch(`api/articles/${id}/comments`);
     const json = await response.json();
     return json;
@@ -13,19 +13,33 @@ export const loadCommentsForArticleId = createAsyncThunk(
 )
 // Create postCommentForArticleId here.
 
-export const commentsSlice = createSlice({
-    name: 'comments',
-    initialState: {
-      // Add initial state properties here.
-      byArticleId: {},
-      isLoadingComments: false,
-      failedToLoadComments: false
-    },
-    // Add extraReducers here.
+export const postCommentForArticleId = createAsyncThunk(
+  'comment/postCommentForArticleId',
+  async ({ articleId, comment }) => {
 
-    extraReducers: (builder) => {
-      builder
-      .addCase(loadCommentsForArticleId.pending, (state,action) => {
+    const requestBody = JSON.stringify({ articleId, comment });
+    const response = await fetch(`api/articles/${articleId}/comments`, { method: 'POST', body: requestBody });
+    const json = await response.json();
+    return json
+
+  }
+)
+
+export const commentsSlice = createSlice({
+  name: 'comments',
+  initialState: {
+    // Add initial state properties here.
+    byArticleId: {},
+    isLoadingComments: false,
+    failedToLoadComments: false,
+    createCommentIsPending: false,
+    failedToCreateComment: false
+  },
+  // Add extraReducers here.
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadCommentsForArticleId.pending, (state, action) => {
         state.isLoadingComments = true;
         state.failedToLoadComments = false;
       })
@@ -36,34 +50,29 @@ export const commentsSlice = createSlice({
       .addCase(loadCommentsForArticleId.fulfilled, (state, action) => {
         state.isLoadingComments = false;
         state.failedToLoadComments = false;
-        state.byArticleId = {[action.payload.articleId]: action.payload.comments};
-        
+        state.byArticleId = { [action.payload.articleId]: action.payload.comments };
       })
-    }
+      .addCase(postCommentForArticleId.pending, (state, action) => {
+        state.createCommentIsPending = true;
+        state.failedToCreateComment = false;
+      })
+      .addCase(postCommentForArticleId.fulfilled, (state, action) => {
+        state.createCommentIsPending = false;
+        state.failedToCreateComment = false;
+        debugger;
+        state.byArticleId[action.payload.articleId].push(action.payload);
 
+      })
+      .addCase(postCommentForArticleId.rejected, (state, action) => {
+        state.createCommentIsPending = false;
+        state.failedToCreateComment = true;
+      })
+  }
 
-    
- /*    extraReducers: {
-      [loadCommentsForArticleId.pending]: (state, action) => {
-        state.isLoadingComments = true;
-        state.failedToLoadComments = false;
-      },
-      [loadCommentsForArticleId.rejected]: (state, action) => {
-        state.isLoadingComments = false;
-        state.failedToLoadComments = true;
-      },
-      [loadCommentsForArticleId.fulfilled]: (state, action) => {
-        state.isLoadingComments = false;
-        state.failedToLoadComments = false;
-        state.byArticleId = action.payload;
-      }
+});
 
-    } */
-  });
-  
-  export const selectComments = (state) => state.comments.byArticleId;
-  export const isLoadingComments = (state) => state.comments.isLoadingComments;
-  export const createCommentIsPending = (state) => state.comments.createCommentIsPending;
-  
-  export default commentsSlice.reducer;
-  
+export const selectComments = (state) => state.comments.byArticleId;
+export const isLoadingComments = (state) => state.comments.isLoadingComments;
+export const createCommentIsPending = (state) => state.comments.createCommentIsPending;
+
+export default commentsSlice.reducer;
